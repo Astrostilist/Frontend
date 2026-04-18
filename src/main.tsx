7c5c65e704f6519e430de6b7b081ed96e4a79e73
Abramenko Ekaterina <katiatgn@yandex.ru>
2026-04-18 22:51:41 +0300

    fix: remove broken i18n import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App.tsx';
-import './i18n/i18n.ts';
 
 const queryClient = new QueryClient();
 
