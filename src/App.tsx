be2095a929db4e4fa8cb586cbd273f031aa76eac
Ekaterina Kharina <Ekaterina.Kharina@waveaccess.ru>
2026-04-27 10:54:12 +0300

    создан элемент загруженного файла и убрано лишнее из app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Dropzone } from './shared/ui/Dropzone';
-import { UploadItem } from './shared/ui/UploadItem/UploadItem';
+import { useEffect } from 'react';
+
 
 function App() {
   const { t, i18n } = useTranslation();
-
-  const [testFile, setTestFile] = useState<File | null>(null);
-
-  const handleAddTestFile = () => {
-    const file = new File(['test content'], 'document_file_name.csv', { type: 'text/plain' });
-    setTestFile(file);
-  };
-
-  const handleRemove = () => {
-    setTestFile(null);
-  };
 
   useEffect(() => {
     localStorage.setItem('token', 'my-test-token');
@@ -23,20 +11,11 @@ function App() {
 
   return (
     <>
+
       <h1>{t('astrostilist')}</h1>
 
       <button onClick={() => i18n.changeLanguage('ru')}>RU</button>
       <button onClick={() => i18n.changeLanguage('en')}>EN</button>
-
-      <Dropzone />
-
-      {/* Кнопка для теста */}
-      <button onClick={handleAddTestFile}>Показать пример файла</button>
-
-      {/* Проверяем, есть ли файл, и показываем его */}
-      {testFile && (
-        <UploadItem file={testFile} onRemove={handleRemove} fileSize={testFile.size} />
-      )}
     </>
   );
 }
