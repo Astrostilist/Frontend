8a154139314e5248329b25228916540985c11cf8
Ekaterina Kharina <Ekaterina.Kharina@waveaccess.ru>
2026-06-28 00:50:41 +0300

    что то со сборкой

--- src/widgets/products/ui/tableProduct.tsx
+++ src/widgets/products/ui/tableProduct.tsx
@@ -13,7 +13,7 @@ import {
 } from '@mui/material';
 import { SearchField } from './components/search';
 import styles from './tableProduct.module.css';
-import  Pagination  from './components/pagination.tsx';
+import Pagination from './components/pagination.tsx';
 import { TagFilter } from './components/tagFilter.tsx';
 import { ProductRow } from './components/productRow.tsx';
 import type { SelectChangeEvent } from '@mui/material/Select';
